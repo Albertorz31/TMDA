e0f66019841e23edb4a4e81ebf847c5ccb0008ac
Albertorz31 <alberto.rodriguez.z@usach.cl>
2021-11-15 15:55:25 -0300

    inicio lab4

---
+++ Lab4/Codigo_Lab4.R
@@ -0,0 +1,5 @@
+require(bnlearn)
+library(bnviewer)
+
+########################### 1. Lectura de Datos ###########################
+url<-"https://raw.githubusercontent.com/Albertorz31/TMDA/main/Lab4/alarm.bif"
